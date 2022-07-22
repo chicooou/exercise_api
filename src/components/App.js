ef388691f22cf402f71a38828a07d5455a13f38b
Francisco Juarez <jojumuz@gmail.com>
2022-07-22 14:10:45 -0600

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 
 import {Route, Routes} from 'react-router-dom'
-import Home from '../components/pages/Home'
+//import Home from '../components/pages/Home'
 import Fetch from '../components/fetch'
 
 
